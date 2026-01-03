bc3828fe4b04e5ba196c01c7809a2070fba5d55e
sarthak6389-sudo <sarthak6389@gmail.com>
2026-01-03 15:23:01 +0000

    add variable declarations and logging examplesg done

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1555
+let accountEmail = "sarthak6389@gmail.com"
+var accountPass = "vesesv"
+accountCity = "Sultanpur"
+console.log(accountId)
+console.log(accountEmail)
+console.table(["ID",accountId,"City",accountCity,"Email",accountEmail])
+// const key word means we can't change
+// let & var - "data can change here"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+/* if declare variable and donot assign variable 
+then java script read it as undefined*/
